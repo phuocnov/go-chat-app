3b83615fc68220966ce524b8fd28ccbdd5ad2788
Phuocnov <phuocnguyenhuu806@gmail.com>
2025-02-06 23:49:16 +0700

    config struct and leveled logs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,24 +1,46 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type Config struct {
+	Addr      string
+	StaticDir string
+}
+
 func main() {
+	cfg := new(Config)
+	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.StaticDir, "static-dir", "./ui/static/", "Path to static assets")
+	flag.Parse()
+
+	// log
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", showSnippet)
 	mux.HandleFunc("POST /snippet/create", createSnippet)
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.StaticDir))
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
+	srv := &http.Server{
+		Addr:     cfg.Addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
 
-	err := http.ListenAndServe(":4000", mux)
+	infoLog.Println("Starting server on ", cfg.Addr)
 
-	log.Fatal(err)
+	err := srv.ListenAndServe()
+
+	errorLog.Fatal(err)
 }
